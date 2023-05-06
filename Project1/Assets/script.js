733d0021ccf800fdd5516a62063c1edf51f4e88b
Sabin budhathoki <budhathokisabin303@gmail.com>
2023-05-06 16:03:14 +0800

    updated code 6/5

--- Project1/Assets/script.js
+++ Project1/Assets/script.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $('.dropdown-trigger').dropdown();
-        
+const citiesEl = document.getElementById('country');
+    const url = 'https://restcountries.com/v2/all';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(country => {
+          const optionEl = document.createElement('option');
+          optionEl.textContent = country.name;
+          citiesEl.appendChild(optionEl);
+        });
+      });
+    const createButton = document.getElementById('create-btn');
+    const containerMain = document.getElementById('container-n');
+    const containerForm = document.getElementById('container-3');
+    createButton.addEventListener('click', function() {
+      containerMain.style.display = 'none';
+      containerForm.style.display = 'block';
+    });
