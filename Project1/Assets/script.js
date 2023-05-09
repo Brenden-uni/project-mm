4d3436ded41ebc0dbc1d117be7f6e9d0c2c2e206
Sabin <sabin@USER-no-Air.hub>
2023-05-09 10:38:49 +0800

    updated with date and time, currency

--- Project1/Assets/script.js
+++ Project1/Assets/script.js
@@ -1,60 +1,95 @@
-const citiesEl = document.getElementById('country');
-    const url = 'https://restcountries.com/v2/all';
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(country => {
-          const optionEl = document.createElement('option');
-          optionEl.textContent = country.name;
-          citiesEl.appendChild(optionEl);
-        });
-      });
-      const createButton = document.getElementById('create-btn');
-      const containerMain = document.getElementById('container-n');
-      const containerForm = document.getElementById('container-3');
-      createButton.addEventListener('click', function() {
-        containerMain.style.display = 'none';
-        containerForm.style.display = 'block';
-      });
-      const profileButton = document.getElementById('profile-btn');
-      const containerProfile = document.getElementById('container-5');
-      profileButton.addEventListener('click', function() {
-        containerMain.style.display = 'none';
-        containerProfile.style.display = 'block';
-      });
-    const submitButtonEl = document.getElementById('submitButtonEl');
-    submitButtonEl.addEventListener('click', function(event) {
-      event.preventDefault();
-      const nameInputEl = document.getElementById('nameInputEl');
-      const incomeInputEl = document.getElementById('incomeInputEl');
-      const budgetInputEl = document.getElementById('budgetInputEl');
-      const countryEl = document.getElementById('country');
-      const name = nameInputEl.value;
-      const income = incomeInputEl.value;
-      const budget = budgetInputEl.value;
-      const country = countryEl.value;
-      const container3El = document.getElementById('container-3');
-      container3El.style.display = 'none';
-      const container4El = document.getElementById('container-4');
-      container4El.style.display = 'block';
-      const welcomeMsgEl = document.getElementById('welcome-msg');
-      welcomeMsgEl.innerHTML = `Welcome <span>${name}</span>, your remaining balance is <span>$${-(income-budget)}</span>`;
-      const informationEl = document.getElementById('information');
-      informationEl.innerHTML = `<div>
-            <h2>In ${country}</h2>
-            <p>Average Income: $999</p>
-            <p>Average Expenses: $999</p>
-        </div>`;
-    });
-  const mainLink = document.getElementById('main-link');
-  mainLink.addEventListener('click', () => {
-    location.reload();
-  });
-  // Get the form and submit button elements
+function displayDateTime() {
+  const now = new Date();
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  const date = now.toLocaleDateString('en-GB', options);
+  const time = now.toLocaleTimeString();
+  const datetime = `${date} ${time}`;
+  return datetime;
+}
+
+const datetimeContainer = document.getElementById("dateandtime");
+datetimeContainer.textContent = displayDateTime();
+
+setInterval(() => {
+  datetimeContainer.textContent = displayDateTime();
+}, 1000);
+
+
+function getCountryInfo() {
+	var country = document.getElementById("country").value;
+	
+	var currencyUrl = "https://restcountries.com/v2/name/" + encodeURI(country) + "?fullText=true";
+	fetch(currencyUrl)
+		.then(response => response.json())
+		.then(data => {
+			var currency = data[0].currencies[0].name;
+			document.getElementById("currency").innerHTML = "Your are from " + country + " and your currency is " +  currency;
+		});
+}
+
+function getCountryList() {
+	var countryListUrl = "https://restcountries.com/v2/all";
+	fetch(countryListUrl)
+		.then(response => response.json())
+		.then(data => {
+			var select = document.getElementById("country");
+			for (var i = 0; i < data.length; i++) {
+				var option = document.createElement("option");
+				option.value = data[i].name;
+				option.text = data[i].name;
+				select.add(option);
+			}
+		});
+}
+
+// Call the getCountryList() function when the page is loaded
+window.onload = getCountryList;
+
+
+const mainLink = document.getElementById('main-link');
+mainLink.addEventListener('click', () => {
+  location.reload();
+});
+
+
+const createButton = document.getElementById('create-btn');
+const containerMain = document.getElementById('container-n');
+const containerForm = document.getElementById('container-3');
+createButton.addEventListener('click', function () {
+  containerMain.style.display = 'none';
+  containerForm.style.display = 'block';
+});
+const profileButton = document.getElementById('profile-btn');
+const containerProfile = document.getElementById('container-5');
+profileButton.addEventListener('click', function () {
+  containerMain.style.display = 'none';
+  containerProfile.style.display = 'block';
+});
+const submitButtonEl = document.getElementById('submitButtonEl');
+submitButtonEl.addEventListener('click', function (event) {
+  event.preventDefault();
+  const nameInputEl = document.getElementById('nameInputEl');
+  const incomeInputEl = document.getElementById('incomeInputEl');
+  const budgetInputEl = document.getElementById('budgetInputEl');
+  const countryEl = document.getElementById('country');
+  const name = nameInputEl.value;
+  const income = incomeInputEl.value;
+  const budget = budgetInputEl.value;
+  const country = countryEl.value;
+  const container3El = document.getElementById('container-3');
+  container3El.style.display = 'none';
+  const container4El = document.getElementById('container-4');
+  container4El.style.display = 'block';
+  const welcomeMsgEl = document.getElementById('welcome-msg');
+  welcomeMsgEl.innerHTML = `Welcome <span><b>${name}</b></span>, <br> Your income is <span><b>${income}</b></span> <br>Your budget  is <span><b>${budget}</b></span>`;
+  const informationEl = document.getElementById('information');
+});
+
+// Get the form and submit button elements
 const form = document.querySelector('#container-3 form');
 const submitButton = document.querySelector('#submitButtonEl');
 // Add an event listener to the submit button
-submitButton.addEventListener('click', function(event) {
+submitButton.addEventListener('click', function (event) {
   // Prevent the default form submission behavior
   event.preventDefault();
   // Get the values from the form inputs
@@ -88,3 +123,153 @@ submitButton.addEventListener('click', function(event) {
       alert('Error retrieving time and date. Please try again later.');
     });
 });
+
+const nameInputEl = document.getElementById('nameInputEl')
+const incomeInputEl = document.getElementById('incomeInputEl')
+const budgetInputEl = document.getElementById('budgetInputEl')
+const addExpenseBtnEl = document.getElementById('addExpenseBtn')
+const formEl = document.getElementById('formEl')
+const tableEl = document.querySelector('table');
+const tbodyEl = document.querySelector('tbody');
+const subtotalEl = document.getElementById("subtotal");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  let elems = document.querySelectorAll('.collapsible');
+  let instances = M.Collapsible.init(elems, {
+    accordion: true
+  });
+});
+
+// Create Function to add user input to a table
+
+function addTable(e) {
+
+  e.preventDefault();
+
+  let date = document.getElementById('dateEl').value;
+  let category = document.getElementById('categoryEl').value;
+  let amount = document.getElementById('amountEl').value;
+
+  tbodyEl.innerHTML += `
+  <tr>
+      <td>${date}</td>
+      <td>${category}</td>
+      <td>${amount}</td>
+      <td><button class="removeBtn">remove</button></td>
+  </tr>
+  `;
+
+  function deleteRow(e) {
+    if (!e.target.classList.contains('removeBtn')) {
+      return
+    }
+
+    const btn = e.target;
+    btn.closest('tr').remove();
+    calculateSubtotal()
+
+  }
+
+  tableEl.addEventListener('click', deleteRow);
+
+  calculateSubtotal()
+
+}
+
+//Add event when user clicks add it will add into table
+
+formEl.addEventListener("submit", addTable)
+
+//Create a function for table row to dynamically add cost of expenses 
+
+function calculateSubtotal() {
+  let subtotal = 0;
+  let tableRows = document.querySelectorAll("#tableEl tr");
+
+  for (let i = 0; i < tableRows.length; i++) {
+    let amountCell = tableRows[i].querySelectorAll("td")[2];
+    let amount = parseFloat(amountCell.textContent);
+    subtotal += amount;
+  }
+
+  let subtotalSpan = document.getElementById("subtotalSpan");
+  subtotalSpan.textContent = "Subtotal: " + subtotal.toFixed(2);
+}
+
+//log out function
+
+document.getElementById('logout-btn').addEventListener('click', () => {
+  localStorage.removeItem('selectedProfile');
+  window.location.href = './index.html';
+});
+
+// get the required elements
+const createBtn = document.getElementById("create-btn");
+const profileBtn = document.getElementById("profile-btn");
+const container3 = document.getElementById("container-3");
+const container4 = document.getElementById("container-4");
+
+// create an empty array to store the profiles
+let profiles1 = [];
+
+// handle submit button click
+submitButtonEl.addEventListener("click", (event) => {
+  // prevent the default form submission behavior
+  event.preventDefault();
+  // get the values entered by the user
+  const name = nameInputEl.value.trim();
+  const income = incomeInputEl.value.trim();
+  const budget = budgetInputEl.value.trim();
+  const country = countryEl.value;
+  // validate the inputs
+  if (!name || !income || !budget || !country) {
+    alert("Please enter all the required fields.");
+    return;
+  }
+  if (isNaN(income) || isNaN(budget)) {
+    alert("Please enter a valid number for income and budget.");
+    return;
+  }
+  // create a new profile object
+  const profiles = {
+    name,
+    income: +income,
+    budget: +budget,
+    country,
+    expenses: []
+  };
+  // add the profile to the array of profiles
+  profiles.push(profiles1);
+  // show the main screen and hide the create profile screen
+  container3.style.display = "none";
+  document.getElementById("container-n").style.display = "flex";
+});
+
+// handle profile button click
+profileBtn.addEventListener("click", () => {
+  // hide the main screen and show the profile list screen
+  document.getElementById("container-n").style.display = "none";
+  document.getElementById("container-5").style.display = "block";
+});
+
+// handle select profile button click
+const selectProfileBtns = document.querySelectorAll(".select");
+selectProfileBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    // set the selected profile in local storage
+    localStorage.setItem("selectedProfile", JSON.stringify(profiles[index]));
+    // show the main screen and hide the profile list screen
+    document.getElementById("container-5").style.display = "none";
+    container4.style.display = "block";
+    // display the name and budget of the selected profile
+    document.querySelector("#welcome-msg span").textContent = profiles1[index].name;
+    document.querySelector("#welcome-msg span:last-child").textContent = profiles1[index].budget;
+  });
+});
+
+// Get a reference to the navbar element
+const navbar = document.querySelector('nav');
+
+// Get the name of the selected country from the profile form
+const country = document.getElementById('country').value;
